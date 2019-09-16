ec5e1b18aa1f7fbc72110821fd21c08648dca751
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-16 02:10:05 -0500

    MVP complete

--- user/userRouter.js
+++ user/userRouter.js
@@ -45,7 +45,7 @@ router.post('/login', (req, res) => {
                 res.status(200).json({ message: `Welcome ${user.username}`})
             } else {
                 res.status(401).json({ message: `Invalid login attempt`})
-            }
+            } 
         })
 })
 
