fcaba7d37639bea4de6ab4aa85cf356b3cf1ade8
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-16 01:18:41 -0500

    writing request handlers

--- user/userRouter.js
+++ user/userRouter.js
@@ -1,4 +1,36 @@
 const express = require('express'); 
-const restricted = require('../auth/middleware-restricted'); 
+const restricted = require('../auth/middleware-restricted');  
+const bcrypt = require('bcryptjs'); 
 
 const Users = require('../db-helper'); 
+
+const router = express.Router(); 
+
+
+//get requests
+router.get('/users', restricted, (req, res) => {
+    Users.getUsers()
+        .then(users => {
+            res.status(200).json(users); 
+        })
+        .catch(err => {
+            res.status(500).json(err); 
+        })
+})
+
+//post requests
+router.post('/register', (req, res) => {
+    Users.addUser()
+        .then(user => {
+            res.status(201).json(user); 
+        })
+        .catch(err => {
+            res.status(500).json(err); 
+        })
+})
+
+router.post('/login', (req, res) => {
+    Users.
+})
+
+module.exports = router; 
